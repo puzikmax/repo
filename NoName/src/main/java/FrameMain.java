e12ca9c03fd919af261cc169a4285a9be7f1551a
puzikmax <puziikmax@gmail.com>
2022-01-27 18:46:22 +0100

    попытка сделать окно поиска

--- NoName/src/main/java/FrameMain.java
+++ NoName/src/main/java/FrameMain.java
@@ -41,12 +41,18 @@ public class FrameMain {
 >>>>>>> 87398a6 (Убрана аннотация)
             public void actionPerformed(ActionEvent e) {
                 JFrame frame = new JFrame("Добавление пациента");
-                frame.pack();
                 JTextField name, lastName, birgDate, addr;
                 name = new JTextField(20);
                 name.setToolTipText("Введите имя");
                 lastName = new JTextField(20);
                 lastName.setToolTipText("Введите фамилию");
+                //byte c;
+                birgDate = new JTextField(8);
+                birgDate.getText();
+                birgDate.setToolTipText("Дато рождения");
+                addr = new JTextField(20);
+                addr.setToolTipText("Адрес");
+
 
                 JLabel labName = new JLabel("Имя");
 
@@ -57,6 +63,8 @@ public class FrameMain {
                 JPanel contents = new JPanel(new FlowLayout(FlowLayout.LEFT));
                 contents.add(name);
                 contents.add(lastName);
+                contents.add(birgDate);
+                contents.add(addr);
                 frame.setContentPane(contents);
                 contents.setSize(400, 130);
                 contents.setVisible(true);
@@ -76,6 +84,13 @@ public class FrameMain {
                 "Найти в базе");
         submenu.add(menuItemOne);
         menuOne.add(submenu);
+        menuOne.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                patientFind pac = new patientFind();
+
+            }
+        });
 
 
         //Second menu in the menu bar.
@@ -86,5 +101,44 @@ public class FrameMain {
         menuBar.add(menuOne);
 
         Frame.setJMenuBar(menuBar);
+    }
+    static class patientFind {
+        patientFind(){
+            JFrame frame = new JFrame("Добавление пациента");
+            JTextField name, lastName, birgDate, addr;
+            name = new JTextField(20);
+            name.setToolTipText("Введите имя");
+            lastName = new JTextField(20);
+            lastName.setToolTipText("Введите фамилию");
+            //byte c;
+            birgDate = new JTextField(8);
+            birgDate.getText();
+            birgDate.setToolTipText("Дато рождения");
+            addr = new JTextField(20);
+            addr.setToolTipText("Адрес");
+
+
+            JLabel labName = new JLabel("Имя");
+
+            labName.setVisible(true);
+            labName.setLocation(100,100);
+            JLabel labLastName = new JLabel("Фамилия");
+
+            JPanel contents = new JPanel(new FlowLayout(FlowLayout.LEFT));
+            contents.add(name);
+            contents.add(lastName);
+            contents.add(birgDate);
+            contents.add(addr);
+            frame.setContentPane(contents);
+            contents.setSize(400, 130);
+            contents.setVisible(true);
+
+
+            frame.setSize(800, 300);
+            frame.setLocation(450, 150);
+            frame.setResizable(false);
+            frame.setVisible(true);
+            frame.pack();
+        }
     }
 }
